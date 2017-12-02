ec4adc9447ef36ca639848b1a1db66e1c39da531
domphan <domphan@users.noreply.github.com>
2017-12-02 10:44:45 -0800

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs290_phando',
-  password        : '7900',
-  database        : 'cs290_phando'
+  user            : '',
+  password        : '',
+  database        : ''
 });
 
 module.exports.pool = pool;
